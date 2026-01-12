b906e7f4a301654e2d2396a83db7bee862fb21a8
F88 <685250+F88@users.noreply.github.com>
2026-01-12 16:29:32 +0900

    refactor(validation): move parameter validation schemas to a new module

--- lib/repository/utils/index.ts
+++ lib/repository/utils/index.ts
@@ -1,4 +1,2 @@
 export { convertFetchResult } from './convert-fetch-result.js';
 export { convertStoreResult } from './convert-store-result.js';
-export { prototypeIdSchema, sampleSizeSchema } from './param-validators.js';
-export { RepositoryValidator } from './validators.js';
